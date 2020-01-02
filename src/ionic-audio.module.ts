2e14513f84fbc02eb362762a97fb6614e2ed9a64
Georgi Georgiev <g.georgiev@rso.bg>
2020-01-02 11:36:23 +0200

    initial port steps

--- src/ionic-audio.module.ts
+++ src/ionic-audio.module.ts
@@ -1,6 +1,6 @@
 import { NgModule, ModuleWithProviders, Optional, CUSTOM_ELEMENTS_SCHEMA }           from '@angular/core';
 import { CommonModule }       from '@angular/common';
-import { IonicModule } from 'ionic-angular';
+import { IonicModule } from '@ionic/angular';
 
 import {AudioTrackComponent} from './ionic-audio-track-component';
 import {AudioTrackProgressComponent, AudioTrackProgressBarComponent} from './ionic-audio-track-progress-component';
@@ -12,12 +12,12 @@ import {AudioPlaylistItemDirective} from './ionic-audio-playlist-item-directive'
 export function declarations() {
   return [
     AudioTrackComponent,
-    AudioTrackProgressComponent, 
+    AudioTrackProgressComponent,
     AudioTrackProgressBarComponent,
     AudioTrackPlayComponent,
     AudioTimePipe,
     AudioPlaylistItemDirective
-  ]; 
+  ];
 }
 
 @NgModule({
@@ -27,7 +27,7 @@ export function declarations() {
   providers:    [],
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
-export class IonicAudioModule { 
+export class IonicAudioModule {
   /**
    * Configures Ionic Audio to use either Cordova or HTML5 audio.
    * If no ```audioProvider``` is set it will automatically choose one based on the current environment
